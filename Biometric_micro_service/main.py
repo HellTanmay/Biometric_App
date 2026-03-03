f23bb967de283c75ec5f7ad0c4165661d363803d
BlxrryFxce17 <akashvmsn@gmail.com>
2026-03-03 14:23:51 +0530

    merged Login and Biometrics

--- Biometric_micro_service/main.py
+++ Biometric_micro_service/main.py
@@ -10,22 +10,25 @@ app = FastAPI()
 EMBEDDING_FOLDER = "embeddings"
 THRESHOLD = 0.6  # Lower = stricter matching
 
+# Ensure embedding folder exists
+if not os.path.exists(EMBEDDING_FOLDER):
+    os.makedirs(EMBEDDING_FOLDER)
 
 # Utility: Extract embedding from image
-def extract_embedding(image_bytes):
-    image = face_recognition.load_image_file(image_bytes)
+def extract_embedding(upload_file: UploadFile):
+    upload_file.file.seek(0)  
+    image = face_recognition.load_image_file(upload_file.file)
     encodings = face_recognition.face_encodings(image)
 
-    if len(encodings) == 0:
-        return None
+    return encodings[0] if encodings else None
 
-    return encodings[0]
-
-#Enroll User
+# =========================
+# 1️⃣ ENROLL USER
+# =========================
 @app.post("/enroll")
 async def enroll(user_id: str = Form(...), file: UploadFile = File(...)):
 
-    embedding = extract_embedding(file.file)
+    embedding = extract_embedding(file)
 
     if embedding is None:
         return {"status": "error", "message": "No face detected"}
@@ -41,23 +44,26 @@ async def enroll(user_id: str = Form(...), file: UploadFile = File(...)):
 async def verify(user_id: str = Form(...), file: UploadFile = File(...),stored_embedding: str = Form(...)):
 
     
-    live_embedding = extract_embedding(file.file)
+    live_embedding = extract_embedding(file)
 
     if live_embedding is None:
         return {"status": "error", "message": "No face detected"}
+    
+    # Convert stored embedding string back to numpy array
     stored_embedding = np.array(json.loads(stored_embedding))
-
-
+    
     # Compare
     distance = np.linalg.norm(stored_embedding - live_embedding)
 
     if distance < THRESHOLD:
         return {
             "status": "success",
+            "match": True,
             "distance": float(distance)
         }
     else:
         return {
             "status": "no_match",
+            "match": False,
             "distance": float(distance)
         }
